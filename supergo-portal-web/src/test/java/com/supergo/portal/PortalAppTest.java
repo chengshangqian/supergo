cf428396403daab7c18fd0f5cec02197bab5cec7
chengshangqian <chengshangqian@126.com>
2020-10-22 22:20:03 +0800

    修正了错误的提交

--- supergo-portal-web/src/test/java/com/supergo/portal/PortalAppTest.java
+++ supergo-portal-web/src/test/java/com/supergo/portal/PortalAppTest.java
@@ -2,12 +2,10 @@ package com.supergo.portal;
 
 import com.supergo.portal.dao.ItemRepository;
 import com.supergo.portal.entity.Item;
-import org.apache.commons.math.stat.descriptive.summary.Product;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.IndexOperations;
 
@@ -16,11 +14,10 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 class PortalAppTest {
 
     // 打印日志
-    public static final Logger LOGGER = LoggerFactory.getLogger(PortalAppTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PortalAppTest.class);
 
     @Autowired
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
@@ -29,10 +26,10 @@ class PortalAppTest {
     private ItemRepository itemRepository;
 
     @Test
-    public void createIndex(){
+    void createIndex(){
         LOGGER.debug("准备创建索引...");
         LOGGER.debug("获取IndexOperations...");
-        IndexOperations ops = elasticsearchRestTemplate.indexOps(Product.class);
+        IndexOperations ops = elasticsearchRestTemplate.indexOps(Item.class);
         boolean exists = ops.exists();
         LOGGER.debug("索引是否已存在？ : {}",exists);
         if(!exists){
@@ -44,7 +41,7 @@ class PortalAppTest {
     }
 
     @Test
-    public void addDocument() {
+    void addDocument() {
         //创建对象
         Item item = new Item();
         item.setId(1L);
@@ -54,38 +51,42 @@ class PortalAppTest {
 
         //保存对象 -> 保存到ES中
         Item saveItem = itemRepository.save(item);
-        assertTrue(null != saveItem);
+        assertNotNull(saveItem);
         LOGGER.debug("创建文档成功？ : {}", saveItem);
     }
 
     @Test
     //查询
-    public void findDocument() {
+    void findDocument() {
         //springData的单体查询,结果会封装到Optional对象
-        Optional<Item> result = itemRepository.findById( 1l);
+        Optional<Item> result = itemRepository.findById( 1L);
         //取出封装的查询结果
-        Item item = result.get();
-        LOGGER.debug("查询文档成功？ : {}", item);
+        if(result.isPresent()){
+            Item item = result.get();
+            LOGGER.debug("查询文档成功？ : {}", item);
+        }
     }
 
     @Test
-    public void updateDocument() {
-        Optional<Item> result = itemRepository.findById( 1l);
-        Item item = result.get();
+    void updateDocument() {
+        Optional<Item> result = itemRepository.findById( 1L);
+        if(result.isPresent()){
+            Item item = result.get();
+            LOGGER.debug("查询文档成功？ : {}", item);
+            LOGGER.debug("查询文档成功？ : {}", item);
 
-        LOGGER.debug("查询文档成功？ : {}", item);
+            item.setItemName("华为Nova7pro");
+            item.setItemPrice(new BigDecimal(3999.99D));
 
-        item.setItemName("华为Nova7pro");
-        item.setItemPrice(new BigDecimal(3999.99D));
-
-        //记录存在就是修改, 不存在就是添加
-        Item updateItem = itemRepository.save(item);
-        LOGGER.debug("更新文档成功？ : {}", updateItem);
+            //记录存在就是修改, 不存在就是添加
+            Item updateItem = itemRepository.save(item);
+            LOGGER.debug("更新文档成功？ : {}", updateItem);
+        }
     }
 
     @Test
-    public void deleteDocument() {
-        itemRepository.deleteById(1l);
+    void deleteDocument() {
+        itemRepository.deleteById(1L);
         LOGGER.debug("删除文档成功");
     }
 }
